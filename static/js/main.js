0b0e9c52ffad81ded979dd18e77cebdd723708a9
Steve King Jr <stevekx86@gmail.com>
2017-02-25 15:38:53 -0700

    Don't load the schedule dynamically

--- static/js/main.js
+++ static/js/main.js
@@ -13,84 +13,6 @@ var globals = globals || {};
         }
     };
 
-    var getStylesheets = function getStylesheets() {
-        $.getJSON("static/res/stylesheets.json", function downloadedStylesheets(data) {
-            head = document.querySelectorAll('head')[0];
-            for(var i = 0; i < data.length; i++) {
-                var elem = document.createElement('link');
-                elem.rel = "stylesheet";
-                elem.type = "text/css";
-                elem.href = "static/css/" + data[i].url;
-                head.appendChild(elem);
-            }
-        });
-    };
-
-    // var getSponsors = function getSponsors() {
-    //     var createSponsor = function createSponsor(tgt, data, i) {
-    //         if(!data[i].img) {
-    //             console.log("Please fix: " + data[i] + " (" + data[i].company + ")");
-    //             return;
-    //         }
-
-
-    //         var link = document.createElement('a');
-    //         var img = document.createElement('img');
-    //         link.className = 'sponsor';
-    //         if(data[i].href)
-    //             link.href = data[i].href;
-    //         link.target = '_blank';
-
-    //         img.src = 'static/img/sponsor_logos/' + data[i].img;
-    //         img.alt = data[i].desc;
-    //         img.className = data[i].class;
-
-    //         link.appendChild(img);
-    //         tgt.appendChild(link);
-    //     };
-
-    //     $.getJSON("static/res/sponsors.json", function downloadedSponsors(data) {
-    //         var target = document.querySelectorAll("#sponsors-flex")[0];
-
-    //         for(var i = 0; i < data.length; i++) {
-    //             createSponsor(target, data, i);
-    //         }
-    //     });
-    // };
-
-    var getSchedule = function getSchedule() {
-        $.getJSON("static/res/schedule.json", function downloadedSchedule(data) {
-            var schedule = document.getElementById("schedule");
-            //For each day
-            var tbl = document.createElement("table");
-            tbl.className = "schedule-tbl";
-
-            for(var i = 0; i < data.length; i++) {
-                //Create elements
-                var hdr = document.createElement("tr");
-                var hdr_e = document.createElement("th");
-                //Set the characteristics
-                hdr_e.innerHTML = data[i].day;
-                hdr_e.colSpan = "2";
-                //Append the elements onto each other
-                hdr.appendChild(hdr_e);
-                tbl.appendChild(hdr);
-
-                for(var j = 0; j < data[i].schedule.length; j++) {
-                    var row = document.createElement("tr");
-                    var tm = document.createElement("td");
-                    var ds = document.createElement("td");
-                    tm.innerHTML = data[i].schedule[j].time;
-                    ds.innerHTML = data[i].schedule[j].desc;
-                    row.appendChild(tm);
-                    row.appendChild(ds);
-                    tbl.appendChild(row);
-                }
-            }
-            schedule.appendChild(tbl);
-        });
-    };
-
     var enableStickyNavbar = function enableStickyNavbar() {
     };
 
@@ -127,9 +49,7 @@ var globals = globals || {};
     //Initialize the website
     $(document).ready(function doInit() {
         linkFaqs();
-        // getSponsors();
         getLinks();
-        getSchedule();
 
         //Set the email addresses after a timeout. Kills spambots :)
         setTimeout(function setEmailAddresses() {
