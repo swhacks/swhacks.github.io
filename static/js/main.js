483ebd6041711de583ef1dde08c3580d57352b04
Dev Lodha <lodhad@gmail.com>
2017-03-11 22:49:56 -0700

    Created notifications system

--- static/js/main.js
+++ static/js/main.js
@@ -24,6 +24,30 @@ var globals = globals || {};
         }, 1000, 'swing');
     }
 
+    var notifs_enabled = true;
+    globals.enableNotifications = globals.enableNotifications || function enableNotifications() {
+        $("#notifications #toggleNotifications").html("Notifications Enabled. Click to Disable");
+        notifs_enabled = true;
+    };
+    globals.disableNotifications = globals.disableNotifications || function disableNotifications() {
+        $("#notifications #toggleNotifications").html("Notifications Disabled. Click to Enable");
+        notifs_enabled = false;
+    };
+    globals.toggleNotifications = globals.toggleNotifications || function toggleNotifications() {
+        notifs_enabled = !notifs_enabled;
+        if(notifs_enabled)
+            globals.enableNotifications();
+        else
+            globals.disableNotifications();
+    };
+    globals.notify = globals.notify || function notify() {
+        if(notifs_enabled) {
+            var audio = document.querySelectorAll("#notifications #notif-audio")[0];
+            audio.play();
+            console.log("Played Notification");
+        }
+    };
+
     //Initialize the website
     $(document).ready(function doInit() {
         linkFaqs();
@@ -32,11 +56,22 @@ var globals = globals || {};
         setTimeout(function setEmailAddresses() {
             $(".email-btn").attr("href", "mailto" + ":tea" + "m@swhacks.io");
         }, 1000);
+
+        globals.prev = globals.prev || null;
+    
+        var refresh = function refresh() {
+            jQuery.get("https://speednotif.devyashis.me", function down(success) {
+                $("#notifications .notif-tgt").html(success);
+                str = success;
+                if(globals.prev && str != globals.prev) {
+                    globals.notify();
+                }
+                globals.prev = str;
+            });
+//            $("#notifications .notif-tgt").load("https://speednotif.devyashis.me");
+        };
+        setInterval(refresh, 1000);
     });
 
-    var refresh = function refresh() {
-        $("#notifications .notif-tgt").load("https://speednotif.devyashis.me");
-    };
-
-    setInterval(refresh, 1000);
+    globals.hack = true;
 })();
